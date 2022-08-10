14289ea43d30963297d683ec24729ed9f430aa77
Thomas Barnholdt <tnb@nine.dk>
2022-08-10 12:49:43 +0200

    ERF-7556 tekstnøgler på forsiden

--- src/components/content/Forside.vue
+++ src/components/content/Forside.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="forside col-lg-7">
-    <h2 class="h1">{{ title }}</h2>
-    <div class="mt-8">
-      Svar på 3 spørgsmål, og få en vurdering af, hvilken virksomhedsform du kan vælge.
-      <br /><br />
-      Guiden er kun vejledende, og vi anbefaler, at du søger råd og vejledning, når du er i tvivl.
-    </div>
+    <h2 class="h1">{{ $t('vaelg-virksomhedsform.forside.title') }}</h2>
+    <div class="mt-8">{{ $t('vaelg-virksomhedsform.forside.manchet') }}</div>
     <div class="mt-4">
-      <button class="button button-primary" @click="$emit('start')">Næste</button>
+      <button class="button button-primary" @click="$emit('start')">{{ $t('vaelg-virksomhedsform.forside.button') }}</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { GuideStep } from '../../enums/guideStep.enum';
-import { titles } from '../../utils/title.util';
-
 export default {
-  name: 'Forside',
-
-  data() {
-    return {
-      title: titles[GuideStep.FORSIDE]
-    };
-  }
+  name: 'Forside'
 };
 </script>
--- src/utils/title.util.ts
+++ src/utils/title.util.ts
@@ -1,11 +1,9 @@
 import { GuideStep } from '../enums/guideStep.enum';
 
-export const titles: { [key in GuideStep]: string } = {
-  [GuideStep.FORSIDE]: 'Guide: Vælg virksomhedsform',
+export const titles: { [key in Exclude<GuideStep, GuideStep.FORSIDE | GuideStep.RESULTAT>]: string } = {
   [GuideStep.STEP_1]: '1. Hvor mange ejere skal din virksomhed have?',
   [GuideStep.STEP_2_EN]: '2. Vil du hæfte personligt for virksomhedens eventuelle gæld?',
   [GuideStep.STEP_2_FLERE]: '2. Vil I hæfte personligt for virksomhedens eventuelle gæld?',
   [GuideStep.STEP_3_JA]: '3. Forventer du, at virksomheden skal omsætte for mere end 50.000 kr. om året?',
-  [GuideStep.STEP_3_NEJ]: '3. Hvor stort et beløb kan du indskyde som selskabskapital?',
-  [GuideStep.RESULTAT]: ''
+  [GuideStep.STEP_3_NEJ]: '3. Hvor stort et beløb kan du indskyde som selskabskapital?'
 };
